fbb606fc649875930d2f8d7a7c90a860adb55cf4
Phong993 <iphong993@gmail.com>
2017-04-13 10:19:42 +0700

    IPHONG993 COMMITED

--- CommonTest/CommonTest/Form1.Designer.cs
+++ CommonTest/CommonTest/Form1.Designer.cs
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grpBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbIP = new System.Windows.Forms.Label();
+            this.grpBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpBox1
+            // 
+            this.grpBox1.Controls.Add(this.lbIP);
+            this.grpBox1.Controls.Add(this.button1);
+            this.grpBox1.Location = new System.Drawing.Point(-1, 163);
+            this.grpBox1.Name = "grpBox1";
+            this.grpBox1.Size = new System.Drawing.Size(282, 100);
+            this.grpBox1.TabIndex = 0;
+            this.grpBox1.TabStop = false;
+            this.grpBox1.Text = "ByIPHONG993";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(275, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me!!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbIP
+            // 
+            this.lbIP.AutoSize = true;
+            this.lbIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIP.Location = new System.Drawing.Point(57, 23);
+            this.lbIP.Name = "lbIP";
+            this.lbIP.Size = new System.Drawing.Size(162, 16);
+            this.lbIP.TabIndex = 1;
+            this.lbIP.Text = "Coded By IPHONG993";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.grpBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.grpBox1.ResumeLayout(false);
+            this.grpBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbIP;
     }
 }
 
--- CommonTest/CommonTest/Form1.cs
+++ CommonTest/CommonTest/Form1.cs
@@ -16,5 +16,10 @@ namespace CommonTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Coded by IPHONG993");
+        }
     }
 }
